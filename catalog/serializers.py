cdc477a4fe33de1130e4ebb5f9b34afbc91fcb5b
ViVaLaFlame <amir.kakharov2001@gmail.com>
2026-06-10 14:20:26 +0300

    fix register

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -4,16 +4,16 @@ from django.contrib.auth.models import User
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = ['username', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
 
         def create(self, validated_data):
-            user = User.objects.create_user(
-                username=validated_data['username'],
-                password=validated_data['password']
-            )
+            user = User(username=validated_data['username'])
+            user.set_password(validated_data['password'])
+            user.save()
             return user
 
 class MaterialSerializer(serializers.ModelSerializer):
